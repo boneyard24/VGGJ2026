ad71c91f25a2a37ffde5a9d55d553558f6078e3b
Corey Bainard <corey.bainard@gmail.com>
2026-01-31 00:57:22 -0800

    progress bar tracking added to qtepopup

--- trainger/Scenes/QTE/qte_popup.gd
+++ trainger/Scenes/QTE/qte_popup.gd
@@ -4,18 +4,27 @@ class_name qte_popup extends Node2D
 @export var MinTime := 1.0
 @export var MaxTime := 5.0
 
+@onready var TimerBar : ProgressBar = $ProgressBar
+var timer_bar_progress := 0.0
+
 signal qte_popup_done(eventSuccess : bool)
 #@onready var timer: Timer = $Timer # Reference the Timer node
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var rng := RandomNumberGenerator.new()
+	var randTime := rng.randf_range(MinTime, MaxTime)
+	
+	TimerBar.min_value = 0
+	TimerBar.max_value = randTime
+	
 	QTETimer.timeout.connect(_on_timer_timeout)
-	QTETimer.start() # Manually start the timer if Auto Start is off
+	QTETimer.start(randTime) # Manually start the timer if Auto Start is off
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	update_progress_bar(delta)
 	
 func _on_timer_timeout():
 	# Code to execute when the timer finishes
@@ -30,4 +39,7 @@ func _input(event: InputEvent) -> void:
 		print("QTE_POPUP TIMER COMPLETED! SUCCESS")
 		qte_popup_done.emit(true)
 	
+func update_progress_bar(delta: float):
+	timer_bar_progress += delta
+	TimerBar.value = timer_bar_progress
 	
--- trainger/coreytest/coreytest.gd
+++ trainger/coreytest/coreytest.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @onready var QtePopup : qte_popup = $QtePopup # Reference the Timer node
 @export var QTEPOP : qte_popup
+@export var progBar : ProgressBar
+
+var progBarVal := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,7 +13,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass 
+	print("value increase: ", delta)
+	progBarVal += delta
+	progBar.value = progBarVal
+	print("value increase: ", progBar.value)
  
 func _on_qteTimer_completed(eventSuccess : bool):
 	print("Timercompleted: ", eventSuccess)

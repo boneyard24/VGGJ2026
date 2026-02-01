f0dd6b3e3fba1496630f32d002c0156093cee1cb
Rodrigo <rodrigorobi@gmail.com>
2026-02-01 00:59:40 -0800

    Added Mixstates to Music

--- trainger/Scenes/Audio/audio_manager.gd
+++ trainger/Scenes/Audio/audio_manager.gd
@@ -1,5 +1,10 @@
 extends Node
 
+# number of fails to trigger the next stage in the music system
+const fails_stage1 = 0
+const fails_stage2 = 1
+
+
 @onready var main_music: AudioStreamPlayer = $Music/MX_MainMusic_Office
 
 @onready var ui_prompt_popup: AudioStreamPlayer = $SFX/UI/UI_Prompt_PopUp
@@ -23,10 +28,73 @@ func play_prompt_win() -> void:
 func play_prompt_lose() -> void:
 	ui_prompt_lose.play()
 
+
+# MUSIC MIX SNAPSHOTS
+
+func music_stage_begin():
+	var stream : AudioStreamSynchronized = main_music.stream
+	stream.set_sync_stream_volume(0, -60)
+	stream.set_sync_stream_volume(1, -16)
+	stream.set_sync_stream_volume(2, -60)
+	stream.set_sync_stream_volume(3, -60)
+	stream.set_sync_stream_volume(4, -60)
+	stream.set_sync_stream_volume(5, -60)
+
+func music_stage_1():
+	var stream : AudioStreamSynchronized = main_music.stream
+	stream.set_sync_stream_volume(0, -60)
+	stream.set_sync_stream_volume(1, -60)
+	stream.set_sync_stream_volume(2, -12)
+	stream.set_sync_stream_volume(3, -14)
+	stream.set_sync_stream_volume(4, -60)
+	stream.set_sync_stream_volume(5, -60)
+
+func music_stage_2():
+	var stream : AudioStreamSynchronized = main_music.stream
+	stream.set_sync_stream_volume(0, -60)
+	stream.set_sync_stream_volume(1, -60)
+	stream.set_sync_stream_volume(2, -12)
+	stream.set_sync_stream_volume(3, -14)
+	stream.set_sync_stream_volume(4, -12)
+	stream.set_sync_stream_volume(5, -60)
+
+func music_stage_gameover():
+	var stream : AudioStreamSynchronized = main_music.stream
+	stream.set_sync_stream_volume(0, -60)
+	stream.set_sync_stream_volume(1, -60)
+	stream.set_sync_stream_volume(2, -60)
+	stream.set_sync_stream_volume(3, -60)
+	stream.set_sync_stream_volume(4, -60)
+	stream.set_sync_stream_volume(5, -16)
+
+func music_stage_fe():
+	var stream : AudioStreamSynchronized = main_music.stream
+	stream.set_sync_stream_volume(0, -12)
+	stream.set_sync_stream_volume(1, -60)
+	stream.set_sync_stream_volume(2, -60)
+	stream.set_sync_stream_volume(3, -60)
+	stream.set_sync_stream_volume(4, -60)
+	stream.set_sync_stream_volume(5, -60)
+
+# MUSIC MIX EVENTS
+
 func update_main_music_qte(current_fails: int) -> void:
 	# main_music.play()
-	# print("AUDIO CURRENT MISTAKES: ", current_fails)
-	return
+	match current_fails:
+		fails_stage1:
+			music_stage_1()
+		fails_stage2:
+			music_stage_2()
+
+func update_main_music_gameover() -> void:
+	music_stage_gameover()
+
+func update_main_music_reset() -> void:
+	music_stage_begin()
+
+func update_main_music_fe() -> void:
+	music_stage_fe()
+
 
 # Very experimental will fail
 func fade_track(sync: AudioStreamSynchronized, track: int, target: float, duration: float):

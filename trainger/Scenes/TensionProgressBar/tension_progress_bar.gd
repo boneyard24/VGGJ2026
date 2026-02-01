7aad8f067bbfa65baae4cd9fca276f8dbc32dd1c
Alina Varela <alina@genpopinteractive.com>
2026-01-31 22:11:45 -0800

    Fixes to train

--- trainger/Scenes/TensionProgressBar/tension_progress_bar.gd
+++ trainger/Scenes/TensionProgressBar/tension_progress_bar.gd
@@ -28,6 +28,6 @@ func UpdateProgress(_progressChange: float):
 func _on_dialogue_box_dialogue_duration_calculated(duration: float) -> void:
 	dialogue_duration = duration
 	if dialogue_duration > 0:
-		velocity = get_viewport().get_visible_rect().size.x - progress_train.texture.get_width() / dialogue_duration
+		velocity = get_viewport().get_visible_rect().size.x / dialogue_duration
 	print("tension_progress_bar.gd received dialogue duration: ", dialogue_duration)
 	print("Velocity set to: ", velocity)

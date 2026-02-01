7e66d7b95608b1ff7870e2641a466afd45b4555b
Alina Varela <alina@genpopinteractive.com>
2026-01-31 20:00:50 -0800

    Train bar progress (still not functional)

--- trainger/Scenes/TensionProgressBar/tension_progress_bar.gd
+++ trainger/Scenes/TensionProgressBar/tension_progress_bar.gd
@@ -1,23 +1,30 @@
 extends Node2D
 
+@onready var progress_train: Sprite2D = $ProgressTrain
 var dialogue_duration: float = 0.0
+var velocity: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var dialogue_box = get_node("res://Dialogue Box/Boss/DialogueBox")
+	var dialogue_box = get_tree().root.get_node("Main/DialogueBox")
 	if dialogue_box:
 		dialogue_box.dialogue_duration_calculated.connect(_on_dialogue_duration_calculated)
+		print ("Dialogue duration: ", dialogue_duration)
 	else:
 		push_warning("Could not find dialogue box node")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if velocity > 0:
+		progress_train.position.x += velocity * delta
 
 func _on_dialogue_duration_calculated(duration: float) -> void:
 	dialogue_duration = duration
+	if dialogue_duration > 0:
+		velocity = get_viewport().get_visible_rect().size.x / dialogue_duration
 	print("Tension bar received dialogue duration: ", dialogue_duration)
+	print("Velocity set to: ", velocity)
 
 func UpdateProgress(progressChange: float):
 	pass

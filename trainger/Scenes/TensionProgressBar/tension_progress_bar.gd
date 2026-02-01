ae3f2981d126ed697572d03acf5870fd2025ff8e
Alina Varela <alina@genpopinteractive.com>
2026-01-31 21:56:24 -0800

    Train now stops when the front touches the right end of the screen

--- trainger/Scenes/TensionProgressBar/tension_progress_bar.gd
+++ trainger/Scenes/TensionProgressBar/tension_progress_bar.gd
@@ -18,7 +18,7 @@ func _process(delta: float) -> void:
 	if velocity > 0:
 		progress_train.position.x += velocity * delta
 		# Stop when train reaches the end of the display
-		if progress_train.position.x - progress_train.texture.get_width() >= get_viewport().get_visible_rect().size.x:
+		if progress_train.position.x >= get_viewport().get_visible_rect().size.x - progress_train.texture.get_width():
 			velocity = 0
 
 func UpdateProgress(_progressChange: float):
@@ -28,6 +28,6 @@ func UpdateProgress(_progressChange: float):
 func _on_dialogue_box_dialogue_duration_calculated(duration: float) -> void:
 	dialogue_duration = duration
 	if dialogue_duration > 0:
-		velocity = get_viewport().get_visible_rect().size.x / dialogue_duration
+		velocity = get_viewport().get_visible_rect().size.x - progress_train.texture.get_width() / dialogue_duration
 	print("tension_progress_bar.gd received dialogue duration: ", dialogue_duration)
 	print("Velocity set to: ", velocity)
